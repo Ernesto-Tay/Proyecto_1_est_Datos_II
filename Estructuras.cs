3545126d65567273a78f5be6c38570fb44f216bc
Ernesto-Tay <JETAYR@correo.url.edu.gt>
2026-09-05 02:06:25 +0000

    Inicialización de clases

---
+++ Estructuras.cs
@@ -0,0 +1,14 @@
+public class Libro
+{
+    public int codigo{get; set;}
+    public string titulo{get; set;}
+    public string autor{get; set;}
+    public string genero {get; set;}
+    public int copias{get; set;}
+    public int veces_prestado{get; set;}
+
+    public void Libro()
+    {
+        
+    }
+}
