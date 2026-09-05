3e669a5af94838b1242e275c6689a768a3903906
ASUS <jetayr@correo.url.edu.gt>
2026-09-04 21:26:10 -0600

    Funciones iniciales para inserción y búsqueda

--- Estructuras.cs
+++ Estructuras.cs
@@ -35,7 +35,7 @@ public class Nodo
     public Libro[] Valores { get; set; } // Arreglo de valores para el nodo
     public bool esHoja { get; set; } // Indica si el nodo es una hoja
 
-    public Nodo(int grado, bool esHoja)
+    public Nodo(int grado, bool esHoja = true)
     {
         this.Claves = new int[2 * grado - 1]; // Inicializa el arreglo de claves
         this.Valores = new Libro[2 * grado - 1]; // Inicializa el arreglo de valores
@@ -44,3 +44,68 @@ public class Nodo
     }
 }
 
+public class ArbolLibros
+{
+    private Nodo raiz; // Raíz del árbol
+    private int orden;
+    private int max_claves;
+
+    public ArbolLibros(int orden)
+    {
+        this.orden = orden;
+        this.max_claves = 2 * orden - 1; // Calcula el número máximo de claves por nodo
+        this.raiz = new Nodo(orden); // Inicializa la raíz del árbol
+    }
+
+
+    public bool Vacio()
+    {
+        return raiz == null; // Indica si el arbol está vacío
+    }
+
+
+    public void Insertar(Libro Libro)
+    {
+        if (raiz == null) // Si la raíz es null, crea un nuevo nodo y lo asigna como raíz
+        {
+            raiz = new Nodo(orden);
+            raiz.Claves[0] = Libro.codigo;
+            raiz.Valores[0] = Libro;
+        }
+        else
+        {
+            if (raiz.Claves[max_claves - 1] != 0) // Si la raíz está llena, se debe dividir
+            {
+                Nodo nuevaRaiz = new Nodo(orden, false);
+                nuevaRaiz.siguiente = raiz;
+                DividirNodo(nuevaRaiz, 0, raiz);
+                raiz = nuevaRaiz;
+            }
+            InsertarNoLleno(raiz, Libro); // Inserta el libro en un nodo que no está lleno
+        }
+
+    }
+
+
+    public bool Buscar(int codigo)
+    {
+        return BuscarRecursivo(raiz, codigo); // Llama a la función recursiva para buscar el libro
+    }
+    private bool BuscarRecursivo(Nodo nodo, int codigo)
+    {
+        if (nodo == null) // Si el nodo es null, retorna false
+            return false;
+
+        int i = 0;
+        while (i < nodo.Claves.Length && nodo.Claves[i] != 0 && codigo > nodo.Claves[i]) // Busca la posición del código en el nodo
+            i++;
+
+        if (i < nodo.Claves.Length && nodo.Claves[i] == codigo) // Si encuentra el código, retorna true
+            return true;
+
+        if (nodo.esHoja) // Si es una hoja y no encontró el código, retorna false
+            return false;
+
+        return BuscarRecursivo(nodo.siguiente, codigo); // Llama recursivamente a la función para buscar en el siguiente nodo
+    }
+}
