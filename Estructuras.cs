5018cc8529fec261736472c6e214f0b54af87ad8
ASUS <jetayr@correo.url.edu.gt>
2026-09-05 19:25:14 -0600

    Inserción completa

--- Estructuras.cs
+++ Estructuras.cs
@@ -2,6 +2,7 @@
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using System.Runtime.InteropServices.Swift;
 
 int Aprox(double x) // aproximador
 {
@@ -20,7 +21,7 @@ int Aprox(double x) // aproximador
 // buscar el valor más a la derecha 
 int ins_izq(int[] lista, int buscado)  // obtiene la posición más a la izquierda de todas las istnacias encotradas del valor buscado
 {
-    int max = lista.Length;
+    int max = lista.Count;
     int min = 0;
     while (min < max)
     {
@@ -36,7 +37,7 @@ int ins_izq(int[] lista, int buscado)  // obtiene la posición más a la izquier
 
 int ins_der(int[] lista, int buscado) // obtiene la posición más a la derecha de todas las instancias aparecidas en el valor buscado
 {
-    int max = lista.Length;
+    int max = lista.Count;
     int min = 0;
     while (min < max)
     {
@@ -90,7 +91,7 @@ public class Nodo // nodo para el árbol B+
     public Nodo padre { get; set; }
     public bool esHoja { get; set; }
 
-    public Nodo(int grado, bool esHoja = true)
+    public Nodo(bool esHoja = true)
     {
         this.Claves = new List<int>(); // Inicializa el arreglo de claves
         this.Valores = new List<Nodo>(); // Inicializa el arreglo de valores
@@ -112,7 +113,7 @@ public class ArbolLibros //el mero Árbol B+
     {
         this.orden = orden;
         this.max_claves = Aprox(2 * orden - 1); // Calcula el número máximo de claves por nodo
-        this.raiz = new Nodo(orden); // Inicializa la raíz del árbol
+        this.raiz = new Nodo(); // Inicializa la raíz del árbol
     }
 
 
@@ -136,7 +137,7 @@ public class ArbolLibros //el mero Árbol B+
     {
         Nodo hoja = buscar_hoja(clave);
         int posc = ins_izq(hoja.claves, clave); // utliza la función para llevar a nodos hoja
-        return posc < hoja.claves.Length && hoja.claves[posc] == clave; // si la posición no sobrepasa el tamaño de matriz Y la posición actual apunta a un nodo, retorna True
+        return posc < hoja.claves.Count && hoja.claves[posc] == clave; // si la posición no sobrepasa el tamaño de matriz Y la posición actual apunta a un nodo, retorna True
     }
 
     // ----- INSERCIÓN -----
@@ -146,19 +147,86 @@ public class ArbolLibros //el mero Árbol B+
         {
             return;
         }
-
+        // se obtiene la hoja onde insertar, y se inserta en su posición respectiva
         Nodo hoja = buscar_hoja(clave);
         int posc = ins_der(hoja.claves, clave);
-        List<int> lista = hoja.claves;
-        List<int> izq = lista.GetRange(0, posc);
-        List<int> der = lista.GetRange(posc, lista.Count - posc);
+        List<int> lista = hoja.claves; // se toma la lista
+        List<int> izq = lista.GetRange(0, posc); // parte izquierda
+        List<int> der = lista.GetRange(posc, lista.Count - posc); // parte derecha
+        izq.Insert(clave); // se pone la clave donde debe quedar
+        hoja.claves = izq.AddRange(der); // y se concatena todo
 
-
+        if (hoja.claves.Count > max_claves) DividirNodo(hoja);
+        recalcular(raiz);
     }
 
-    private void DividirNodo(Nodo nodoPadre, int posicion, Nodo nodoHijo) // proceso de división
+    private void DividirNodo(Nodo nodo) // proceso de división
     {
+        p_div = (nodo.claves.count + 1) / 2;
+        p_div = Convert.ToInt32(p_div); // calcula punto de división y lo convierte a entero
 
+        Nodo n_hoja = Nodo();
+        n_hoja.padre = nodo.padre; // comparten papi
+        n_hoja.claves = nodo.claves.GetRange(0, p_div);
+        nodo.claves = nodo.claves.GetRange(p_div, nodo.claves.Count - p_div); // se dividen las claves
+
+        n_hoja.siguiente = nodo.siguiente;
+        nodo.siguiente = n_hoja; // nodo -> nuevo_nodo -> siguiente
+
+        int c_guia = n_hoja.claves[0]; //clave guía para siguientes operatorias
+        ins_padre(nodo, c_guia, n_hoja);
     }
+
+    private void ins_padre(Nodo izq, int guia, Nodo der)
+    {
+        if (izq == raiz) // caso 1 - la raíz se divide
+        {
+            n_raiz = Nodo(false); //nueva raíz
+            n_raiz.claves.Insert(guia);
+            n_raiz.hijos.Insert(izq);
+            n_raiz.hijos.Insert(der); // se actualizan guías y hojas
+
+            // se actualizan referencias
+            izq.padre = n_raiz;
+            der.padre = n_raiz;
+            raiz = n_raiz;
+            return;
+        }
+
+        // recupera padre y localiza hijo dividido
+        Nodo padre = izq.padre;
+        int posicion = padre.hijos.Find(izq);
+
+        // completa información del nuevo padre
+        padre.claves.Insert(posicion, guia);
+        padre.hijos.Insert(posicion + 1, der);
+        der.padre = padre;
+
+        // salvaguarda: si se desborda el papi, se divide
+        if (padre.claves.Count > max_claves) division_interna(padre);
+    }
+
+    private void division_interna(Nodo nodo)
+    {
+        p_div = nodo.claves.count / 2;
+        ascendido = nodo.claves[p_div];
+
+        n_interno = Nodo();
+        n_interno.padre = nodo.padre;
+
+        n_interno.claves = nodo.claves.GetRange(p_div + 1, nodo.claves.Count - p_div - 1);
+        n_interno.hijos = nodo.hijos.GetRange(p_div + 1, nodo.hijos.Count - p_div - 1);
+
+        foreach (Nodo hijo in n_interno.hijos)
+        {
+            hijo.padre = n_interno;
+        }
+
+        nodo.claves = nodo.claves.GetRange(0, p_div);
+        nodo.hijos = nodo.claves.GetRange(0, p_div + 1);
+
+        ins_padre(nodo, ascendido, n_interno);
+    }
+    
 
 }
